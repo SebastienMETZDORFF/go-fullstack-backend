e6f3dff12cd08f6860d15a5b4adb7956f60d57ef
SebastienMETZDORFF <sebmetzdorff@gmail.com>
2024-07-01 14:50:02 +0200

    Vérifier les informations d'identification d'un utilisateur

--- controllers/user.js
+++ controllers/user.js
@@ -17,5 +17,22 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user === null) {
+                return res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' });
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' });
+                    }
+                    return res.status(200).json({
+                        userId: user._id,
+                        token: 'TOKEN'
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
 };
